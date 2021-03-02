b869871fd7d7279bb00e66692540365a36731396
chengjiaxiongkf <66402618+chengjiaxiongkf@users.noreply.github.com>
2021-03-02 14:27:16 +0800

    优化异常处理

--- com-yc-sms/com-yc-sms-core/src/main/java/com/yc/sms/core/util/AliyunSmsUtils.java
+++ com-yc-sms/com-yc-sms-core/src/main/java/com/yc/sms/core/util/AliyunSmsUtils.java
@@ -49,55 +49,50 @@ public class AliyunSmsUtils {
             throw new SmsException(SmsExceptionCodeEnum.PHONE_NULL);
         }
         String bizId = null;
-        try {
-            //设置超时时间-可自行调整
-            System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
-            System.setProperty("sun.net.client.defaultReadTimeout", "10000");
-            //初始化ascClient,暂时不支持多region（请勿修改）
-            IClientProfile profile = DefaultProfile.getProfile(smsRefreshProperties.getRegionId(), smsRefreshProperties.getAccessKeyId(),
-                    smsRefreshProperties.getAccessSecret());
-            DefaultProfile.addEndpoint(smsRefreshProperties.getRegionId(), smsRefreshProperties.getRegionId(), smsRefreshProperties.getProduct(), smsRefreshProperties.getDomain());
-            IAcsClient client = new DefaultAcsClient(profile);
-            SendBatchSmsRequest request = new SendBatchSmsRequest();
-            //使用post提交
-            request.setMethod(MethodType.POST);
-            //修改数据交互格式
-            request.setAcceptFormat(FormatType.JSON);
-            //必填:待发送手机号。支持JSON格式的批量调用，批量上限为100个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
-            JSONArray phoneArray = new JSONArray(telephone);
-            request.setPhoneNumberJson(phoneArray.toJSONString());
-            JSONArray signNameArray = new JSONArray();  //发送消息的签名jsonarray
-            JSONArray msgArray = new JSONArray();       //发送消息中特殊字符的替换jsonarray
-            for (int i=0;i<telephone.size();i++) {
-                signNameArray.add(i,smsRefreshProperties.getSignName());
-                if (message!=null && !"".equals(message)) {
-                    continue;
-                }
-                msgArray.add(i, JSONObject.parseObject("{}"));
+        //设置超时时间-可自行调整
+        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
+        System.setProperty("sun.net.client.defaultReadTimeout", "10000");
+        //初始化ascClient,暂时不支持多region（请勿修改）
+        IClientProfile profile = DefaultProfile.getProfile(smsRefreshProperties.getRegionId(), smsRefreshProperties.getAccessKeyId(),
+                smsRefreshProperties.getAccessSecret());
+        DefaultProfile.addEndpoint(smsRefreshProperties.getRegionId(), smsRefreshProperties.getRegionId(), smsRefreshProperties.getProduct(), smsRefreshProperties.getDomain());
+        IAcsClient client = new DefaultAcsClient(profile);
+        SendBatchSmsRequest request = new SendBatchSmsRequest();
+        //使用post提交
+        request.setMethod(MethodType.POST);
+        //修改数据交互格式
+        request.setAcceptFormat(FormatType.JSON);
+        //必填:待发送手机号。支持JSON格式的批量调用，批量上限为100个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
+        JSONArray phoneArray = new JSONArray(telephone);
+        request.setPhoneNumberJson(phoneArray.toJSONString());
+        JSONArray signNameArray = new JSONArray();  //发送消息的签名jsonarray
+        JSONArray msgArray = new JSONArray();       //发送消息中特殊字符的替换jsonarray
+        for (int i=0;i<telephone.size();i++) {
+            signNameArray.add(i,smsRefreshProperties.getSignName());
+            if (message!=null && !"".equals(message)) {
+                continue;
             }
-            if (message==null || "".equals(message)){
-                message = msgArray.toJSONString();
-            }
-            //必填:短信签名-支持不同的号码发送不同的短信签名
-            request.setSignNameJson(signNameArray.toJSONString());
-            //必填:短信模板-可在短信控制台中找到
-            request.setTemplateCode(templateCode);
-            //必填:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
-            //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
-            request.setTemplateParamJson(message);
-            //请求失败这里会抛ClientException异常
-            SendBatchSmsResponse sendSmsResponse = client.getAcsResponse(request);
-            if(sendSmsResponse.getCode() == null || !sendSmsResponse.getCode().equals("OK")) {
-                //发送失败
-                log.error("消息发送失败,错误码：{};错误内容：{}",sendSmsResponse.getCode(),sendSmsResponse.getMessage());
-                throw new SmsException(SmsExceptionCodeEnum.SEND_FAIL);
-            }
-            //请求成功
-            bizId = sendSmsResponse.getBizId();
-        }catch (ClientException e){
-            log.error(e.getMessage(),e);
-            throw new SmsException(SmsExceptionCodeEnum.SEND_REQUEST_FAIL);
+            msgArray.add(i, JSONObject.parseObject("{}"));
         }
+        if (message==null || "".equals(message)){
+            message = msgArray.toJSONString();
+        }
+        //必填:短信签名-支持不同的号码发送不同的短信签名
+        request.setSignNameJson(signNameArray.toJSONString());
+        //必填:短信模板-可在短信控制台中找到
+        request.setTemplateCode(templateCode);
+        //必填:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+        //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
+        request.setTemplateParamJson(message);
+        //请求失败这里会抛ClientException异常
+        SendBatchSmsResponse sendSmsResponse = client.getAcsResponse(request);
+        if(sendSmsResponse.getCode() == null || !sendSmsResponse.getCode().equals("OK")) {
+            //发送失败
+            log.error("消息发送失败,错误码：{};错误内容：{}",sendSmsResponse.getCode(),sendSmsResponse.getMessage());
+            throw new SmsException(SmsExceptionCodeEnum.SEND_FAIL);
+        }
+        //请求成功
+        bizId = sendSmsResponse.getBizId();
         return bizId;
     }
 
